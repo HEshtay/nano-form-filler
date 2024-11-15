3421dbf9f7970bd758676c0784e022f8e4691929
Hoseen Eshtay <eshtay@doehle.de>
2024-11-15 19:27:38 +0100

    feat | added manifest and first basic implementation of popup

---
+++ popup.js
@@ -0,0 +1,11 @@
+function fillForm() {
+  console.log("Form filled with one click");
+}
+
+function takeScreenshot() {
+  console.log("Screenshot taken");
+}
+
+function useSpeech() {
+  console.log("Speech used");
+}
