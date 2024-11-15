fd05acee3c6e8778f497dd6de061b485966515a1
Felix Herold <felix@herold.solutions>
2024-11-15 21:03:11 +0100

    chore: add DOMContentLoaded event listener

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,11 +1,3 @@
-function fillForm() {
-  console.log("Form filled with one click");
-}
-
-function takeScreenshot() {
-  console.log("Screenshot taken");
-}
-
-function useSpeech() {
-  console.log("Speech used");
-}
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM ready");
+});
