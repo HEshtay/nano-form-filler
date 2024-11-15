cd99e983db26a6b9f5165d435ed73730e0d6bd7c
Felix Herold <felix@herold.solutions>
2024-11-15 21:10:20 +0100

    feat: add button events

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM ready");
+
+    document.getElementById("autoFillFormButton")?.addEventListener("click", () => {
+        console.log("autoFillFormButton");
+    });
+
+    document.getElementById("takeScreenshotButton")?.addEventListener("click", () => {
+        console.log("takeScreenshotButton");
+    });
+
+    document.getElementById("useSpeechButton")?.addEventListener("click", () => {
+        console.log("useSpeechButton");
+    });
 });
