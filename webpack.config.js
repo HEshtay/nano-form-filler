b0c80ec957671222d5d5543a6aebb143fab8996d
Hoseen Eshtay <eshtay@doehle.de>
2024-11-23 22:22:06 +0100

    feat | added background service worker

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     entry: {
         popup: "./src/popup/popup.ts",
         content: "./src/scripts/content.ts",
+        background: "./src/background.ts",
     },
     output: {
         path: path.resolve(__dirname, "dist"),
