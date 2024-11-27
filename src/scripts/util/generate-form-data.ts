62133f6074a7489293fda666b1cf2486e8eef394
Hoseen Eshtay <eshtay@doehle.de>
2024-11-27 15:33:59 +0100

    Feat |  added form structure

--- src/scripts/util/generate-form-data.ts
+++ src/scripts/util/generate-form-data.ts
@@ -1,38 +1,31 @@
-import { SelectedForm } from "../../models/selected-form";
+import { FormStructure } from "./get-selected-form";
 
 function getSystemPrompt(output: Object) {
     return `
         You are a assistant that should help with filling forms automatically.
         You generate data for a form based on the users input.
         Extract the information to a JSON object of this shape:
-        ${JSON.stringify(output)}
+        ${JSON.stringify(output, null, 2)}
 
         If you cant generate the information for a field, leave it empty.
         Answer only with the JSON object.
         Do not add any other text.
+        Stick to the format of the JSON object.
+        For type Radio and checkbox, select one of the options and mark it as checked = true.
         Do only generate values with the latin alphabet, numbers or booleans.
         Do not use any special characters or emojis.
         DO not use the most common languages.
     `;
 }
 
-export async function generateFormData(form: SelectedForm, input?: string) {
-    // format a json object for output
-    const output: Record<string, unknown> = {};
-    const additions: string[] = [];
-    for (const [key, input] of form.entries()) {
-        switch (input.type) {
-            case "checkbox":
-                output[key] = false;
-                break;
-            default:
-                output[key] = "";
-        }
-    }
+export async function generateFormData(form: FormStructure | null, input?: string) {
 
     console.log("Running prompt");
+    if (!form) {
+        return;
+    }
     const session = await window.ai.languageModel.create({
-        systemPrompt: getSystemPrompt(output),
+        systemPrompt: getSystemPrompt(form),
     });
     let reply: string = "";
     // random data on no input
--- src/scripts/util/get-selected-form.ts
+++ src/scripts/util/get-selected-form.ts
@@ -1,24 +1,224 @@
-import { SelectedForm, SelectedFormInput } from "../../models/selected-form";
 import { getForms } from "./get-forms";
 
-export function getSelectedForm(index: number) {
+interface InputOption {
+    value: string;
+    id?: string;
+    label: string;
+    checked?: boolean;
+}
+
+interface BaseInput {
+    type: string;
+    id: string | null;
+    name: string | null;
+    element?: Element;
+}
+
+interface TextInput extends BaseInput {
+    placeholder?: string;
+    value?: string | null;
+}
+
+interface RadioInput extends BaseInput {
+    options: InputOption[];
+}
+
+interface CheckboxInput extends BaseInput {
+    options: InputOption[];
+}
+
+interface SelectInput extends BaseInput {
+    options: InputOption[];
+    selected?: string | number | null;
+}
+
+type FormInput = TextInput | RadioInput | CheckboxInput | SelectInput;
+
+export interface FormStructure {
+    formId: string;
+    inputs: FormInput[];
+}
+
+export function getSelectedForm(index: number): FormStructure | null {
     const forms = getForms();
     const selectedForm = forms[index];
-    const inputs: SelectedForm = new Map<string, SelectedFormInput>();
-    selectedForm.querySelectorAll("input").forEach((input) => {
-        const inputKey = input.name !== "" ? input.name : input.id;
-        if (!inputs.has(inputKey)) {
-            inputs.set(inputKey, {
-                label: input.name ?? "",
-                placeholder: input.placeholder ?? "",
-                type: input.getAttribute("type") || "text",
-                values: [],
-            });
+
+    // Get the path of the selected form
+    let formPath: string | undefined;
+    if (selectedForm && !selectedForm.id) {
+        formPath = getFormPath(selectedForm);
+    }
+
+    const capturedForm = captureFormStructure(selectedForm?.id, formPath);
+    console.log("-------------------------------------------");
+    console.log(JSON.stringify(capturedForm, null, 2));
+    console.log("-------------------------------------------");
+    console.log("Captured Form:", capturedForm);
+    return capturedForm;
+}
+
+// Helper function to get the path of the form
+function getFormPath(form: HTMLFormElement): string {
+    let path = form.tagName.toLowerCase();
+    let parent = form.parentElement;
+    while (parent) {
+        path = `${parent.tagName.toLowerCase()} > ${path}`;
+        parent = parent.parentElement;
+    }
+    return path;
+}
+
+function captureFormStructure(formId?: string, formPath?: string): FormStructure | null {
+    let form: HTMLFormElement | null = null;
+
+    if (formId) {
+        form = document.getElementById(formId) as HTMLFormElement | null;
+    } else if (formPath) {
+        form = document.querySelector(formPath);
+    }
+
+    if (form) {
+        return extractFormStructure(form, formId || formPath || "unknown-form");
+    } else {
+        // If both formId and formPath are undefined, search directly for input elements
+        const inputs = document.querySelectorAll("input, textarea, select");
+
+        if (inputs.length === 0) {
+            console.error("No input elements found.");
+            return null;
         }
-        // if radio add values
-        if (input.type === "radio") {
-            inputs.get(inputKey)?.values.push(input.value);
+
+        const formStructure: FormStructure = {
+            formId: "direct-input-search",
+            inputs: [],
+        };
+
+        extractInputs(inputs, formStructure);
+        console.log("Direct input search:", formStructure);
+        return formStructure;
+    }
+}
+
+function extractFormStructure(form: HTMLFormElement, formIdentifier: string): FormStructure | null {
+    const inputs = form.querySelectorAll("input, textarea, select");
+    if (inputs.length === 0) {
+        console.error("No input elements found in the form.");
+        return null;
+    }
+
+    const formStructure: FormStructure = {
+        formId: form.id || formIdentifier,
+        inputs: [],
+    };
+
+    extractInputs(inputs, formStructure);
+    return formStructure;
+}
+
+function extractInputs(inputs: NodeListOf<Element>, formStructure: FormStructure): void {
+    inputs.forEach((element) => {
+        const { type, id, name, placeholder, value, checked } = element as HTMLInputElement;
+
+        const baseInput: BaseInput = {
+            type: element.tagName.toLowerCase() === "textarea" ? "textarea" : type,
+            id: id || null,
+            name: name || null,
+            element,
+        };
+
+        switch (baseInput.type) {
+            case "text":
+            case "email":
+            case "date":
+            case "number":
+            case "password":
+            case "url":
+            case "tel": {
+                const textInput: TextInput = {
+                    ...baseInput,
+                    placeholder: placeholder || "",
+                    value: value || null,
+                };
+                formStructure.inputs.push(textInput);
+                break;
+            }
+            case "radio": {
+                const radioOption: InputOption = {
+                    value,
+                    id: id,
+                    label: (element as HTMLInputElement).labels?.[0]
+                        ? (element as HTMLInputElement).labels?.[0].textContent || value
+                        : value,
+                    checked: checked || false,
+                };
+
+                const existingRadioGroup = formStructure.inputs.find(
+                    (inp) => inp.type === "radio" && inp.name === name,
+                ) as RadioInput | undefined;
+
+                if (existingRadioGroup) {
+                    existingRadioGroup.options.push(radioOption);
+                } else {
+                    const radioInput: RadioInput = {
+                        ...baseInput,
+                        options: [radioOption],
+                    };
+                    formStructure.inputs.push(radioInput);
+                }
+                break;
+            }
+            case "checkbox": {
+                const checkboxOption: InputOption = {
+                    value,
+                    id: id,
+                    label: (element as HTMLInputElement).labels?.[0]
+                        ? (element as HTMLInputElement).labels?.[0].textContent || value
+                        : value,
+                    checked: checked || false,
+                };
+
+                const existingCheckboxGroup = formStructure.inputs.find(
+                    (inp) => inp.type === "checkbox" && inp.name === name,
+                ) as CheckboxInput | undefined;
+
+                if (existingCheckboxGroup) {
+                    existingCheckboxGroup.options.push(checkboxOption);
+                } else {
+                    const checkboxInput: CheckboxInput = {
+                        ...baseInput,
+                        options: [checkboxOption],
+                    };
+                    formStructure.inputs.push(checkboxInput);
+                }
+                break;
+            }
+            case "select-one": {
+                const selectInput: SelectInput = {
+                    ...baseInput,
+                    type: "select",
+                    options: [],
+                };
+                for (let option of Array.from((element as HTMLSelectElement).options)) {
+                    selectInput.options.push({
+                        value: option.value,
+                        label: option.textContent || "",
+                    });
+                }
+                formStructure.inputs.push(selectInput);
+                break;
+            }
+            case "textarea": {
+                const textInput: TextInput = {
+                    ...baseInput,
+                    placeholder: placeholder || "",
+                    value: value || null,
+                };
+                formStructure.inputs.push(textInput);
+                break;
+            }
+            default:
+                console.warn(`Unsupported input type: ${type}`);
+                break;
         }
     });
-    return inputs;
 }
